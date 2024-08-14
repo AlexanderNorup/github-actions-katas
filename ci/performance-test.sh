9d1088fbc1a052f8545dc6e6a4eed9a6a15703f8
Alexander Nørup <AlexanderNorup@users.noreply.github.com>
2024-08-14 12:51:11 +0200

    Credentials + fix repo-name with /

--- ci/performance-test.sh
+++ ci/performance-test.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-DockerRepo='alexandernorup'
+DockerRepo='alexandernorup/'
 docker_username=$DockerRepo docker compose -f performance-test/docker-compose.yml --project-directory . -p ci up --build --exit-code-from test
